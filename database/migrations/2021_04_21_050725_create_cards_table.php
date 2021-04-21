02db15e647ad8dc94c944a2fb03674bf9476988c
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-04-21 11:48:04 +0530

    Composer Install Datatables

---
+++ database/migrations/2021_04_21_050725_create_cards_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name');
+            $table->text('user_id')->nullable();
+            $table->text('address')->nullable();
+            $table->text('phone_number')->nullable();
+            $table->text('fax_number')->nullable();
+            $table->text('card_type');
+            $table->text('email')->nullable();
+            $table->text('city')->nullable();
+            $table->text('website')->nullable();
+            $table->text('job_role')->nullable();
+            $table->text('company_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
