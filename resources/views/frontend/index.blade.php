98c643500c21d214dc9cd212e1d8476a1efec641
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-04-30 16:41:06 +0530

    Index Details Blades

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,66 +1,289 @@
 
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Index Machine</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <title>SKY CARD</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="{{url('light_theme/plugin/AnimTrap/css/animtrap.css')}}"/>
+    <link rel="stylesheet" href="{{url('light_theme/styles/css/style.css')}}"/>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">OCR</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-
-</nav>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Optical Character Recognition | Image annotation') }}</div>
-
-                <div class="card-body">
-                    <form role="form" method="POST" action="/annotate" enctype="multipart/form-data">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('Upload image') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="image" type="file" class="form-control{{ $errors->has('image') ? ' is-invalid' : '' }}" name="image" value="{{ old('image') }}" required autofocus>
-
-                                @if ($errors->has('image'))
-                                    <span class="invalid-feedback">
-                                              <strong>{{ $errors->first('image') }}</strong>
-                                          </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Submit') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<nav class="navbar-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                <div class="row">
+                    <ul>
+                        <li><a class="navigation-link active" href="#">Home</a></li>
+                        <li><a class="navigation-link" href="#">Sky Cards App</a></li>
+                        <li><a class="navigation-link" href="#">Sky Club</a></li>
+                        <li><a class="navigation-link" href="#">Sky Daily</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-2 d-flex justify-content-center">
+                <img src="{{url('light_theme/assets/logo compressed.png')}}" class="default-logo header-logo" alt="" />
+                <img src="{{url('light_theme/assets/logo compressed.png')}}" class="sticky-logo d-none" alt="" />
+            </div>
+            <div class="col-5">
+                <div class="row">
+                    <ul>
+                        <li><a class="navigation-link" href="#">Sky Card Holder</a></li>
+                        <li><a class="navigation-link" href="#">Contact</a></li>
+                        <li><a class="navigation-link" href="#">About</a></li>
+                        <li>
+                            <a class="navigation-link reg" href="#">Register/Log In</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
-</div>
-</body>
+</nav>
+
+<section id="top-slider">
+    <div class="container">
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-4">
+                <h1 data-animscroll="fade-up">Sky Card App</h1>
+                <h5 data-animscroll="fade-up" data-animscroll-delay="100">
+                    <span>Touchless</span> Smart Business cards Solution
+                </h5>
+                <p data-animscroll="fade-up" data-animscroll-delay="200">
+                    Convert paper business card to digital with a single tap
+                </p>
+                <a class="theam-btn" type="button" href="#" data-animscroll="fade-up" data-animscroll-delay="300">Learn More</a>
+            </div>
+            <div class="col-7">
+                <img data-animscroll="fade-left" src="{{url('light_theme/assets/Group 69.png')}}" style="width: 100%" alt=""/>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="sky-club">
+    <div class="dark-round" data-animscroll="fade-up">
+        <div class="container p-4">
+            <div class="row">
+                <div class="col-6">
+                    <img src="{{url('light_theme/assets/Asset 1skynetwork.png')}}" style="width: 80%" data-animscroll="fade-right"/>
+                </div>
+                <div class="col-6 pl-5" data-animscroll="fade-left">
+                    <h1 data-animscroll="fade-up">Sky <span>Club</span></h1>
+                    <h5 data-animscroll="fade-up" data-animscroll-delay="100">
+                        The largest Network of websites
+                    </h5>
+                    <p data-animscroll="fade-up" data-animscroll-delay="200">
+                        Connect, communicate and share within the largest network of
+                        active websites.
+                        <br />
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                        diam nonumy eirmod tempor invidunt ut labore et dolore
+                    </p>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <a data-animscroll="fade-up" data-animscroll-delay="100" href="#" type="button" class="theam-btn-light">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="light-round" data-animscroll="fade-up" data-animscroll-delay="300"></div>
+</section>
+
+<section id="sky-daily">
+    <div class="container">
+        <div class="row">
+            <div class="col-6" data-animscroll="fade-right">
+                <h1 data-animscroll="fade-up">Sky Daily</h1>
+                <h5 data-animscroll="fade-up" data-animscroll-delay="100">
+                    E-Mail Marketing Solution
+                </h5>
+                <p data-animscroll="fade-up" data-animscroll-delay="200">
+                    Promote your business to Registered and authenticated websites and
+                    members of largest sky cards community.
+                </p>
+                <br/>
+                <br/>
+                <br/>
+                <a data-animscroll="fade-up" data-animscroll-delay="300" href="#" class="theam-btn" type="button">Learn More</a>
+            </div>
+            <div class="col-6" data-animscroll="fade-left">
+                <img src="{{url('light_theme/assets/Group 70.png')}}" style="width: 100%" alt="" />
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="sky-vip">
+    <div class="left-blue" data-animscroll="fade-right" data-animscroll-delay="100"></div>
+    <div class="container vip-container">
+        <div class="row">
+            <div class="col-6">
+                <div class="details">
+                    <h1 data-animscroll="fade-up">Sky <span>VIP</span></h1>
+                    <h5 data-animscroll="fade-up">
+                        Become a member & enjoy exclusive offers from
+                    </h5>
+                    <div class="button-set">
+                        <a data-animscroll="fade-up" data-animscroll-delay="100" href="#" class="theam-btn-light" type="button" style="padding: 10px 50px">Hotels</a>
+                        <a data-animscroll="fade-up" data-animscroll-delay="200" href="#" class="theam-btn-light" type="button" style="padding: 10px 50px">Restaurants</a>
+                        <a data-animscroll="fade-up" data-animscroll-delay="300" href="#" class="theam-btn-light" type="button">Clubs</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <img data-animscroll="fade-left" style="width: 80%" src="{{url('light_theme/assets/Layer 2.png')}}" alt="" />
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="contract-organizer">
+    <div class="container">
+        <div class="row">
+            <div class="col-6" data-animscroll="fade-right">
+                <img src="{{url('light_theme/assets/Group 702.png')}}" style="width: 100%" alt="" />
+            </div>
+            <div class="col-6 pl-5" data-animscroll="fade-left">
+                <h1 data-animscroll="fade-up">Contact Organizer</h1>
+                <h5 data-animscroll="fade-up" data-animscroll-delay="100">
+                    Forget about card holders!
+                </h5>
+                <p data-animscroll="fade-up" data-animscroll-delay="200">
+                    All contacts are stored in a cloud and synchronized across your
+                    devices, allowing access from your smartphone, tablet or computer.
+                </p>
+                <a data-animscroll="fade-up" data-animscroll-delay="300" href="#" class="theam-btn mt-4" type="button">Learn More</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="sky-card-app">
+    <div class="dark-round" data-animscroll="fade-up" data-animscroll-delay="100">
+        <div class="container">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-7 pl-5" data-animscroll="fade-right">
+                    <h1 data-animscroll="fade-up" data-animscroll-delay="100">
+                        Sky Card App
+                    </h1>
+                    <br />
+                    <h5 data-animscroll="fade-up" data-animscroll-delay="200">
+                        Enter business cards into your smartphone
+                    </h5>
+                    <h6 data-animscroll="fade-up" data-animscroll-delay="200">
+                        Fast, easy and accurate.
+                    </h6>
+                    <p data-animscroll="fade-up" data-animscroll-delay="300">
+                        Use your smartphone to scan business cards and save all the data
+                        to your contact list.
+                    </p>
+                    <br />
+                    <br />
+                    <h5 data-animscroll="fade-up" data-animscroll-delay="100">
+                        Share your contacts
+                    </h5>
+                    <h6 data-animscroll="fade-up" data-animscroll-delay="200">
+                        Touchless in 2021
+                    </h6>
+                    <p data-animscroll="fade-up" data-animscroll-delay="300">
+                        Network and share your contact details with just one tap.
+                        Sending an e-mail with your digital business card now only takes
+                        a few seconds.
+                    </p>
+                    <br/>
+                    <div class="row">
+                        <div class="col-4" data-animscroll="fade-up" data-animscroll-delay="300">
+                            <a href="#">
+                                <img src="{{url('light_theme/assets/Mask Group 6.png')}}" style="width: 100%" alt=""/>
+                            </a>
+                        </div>
+                        <div class="col-4" data-animscroll="fade-up" data-animscroll-delay="400">
+                            <a href="#">
+                                <img src="{{url('light_theme/assets/Mask Group 7.png')}}" style="width: 100%" alt=""/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-4" data-animscroll="fade-left">
+                    <img src="{{url('light_theme/assets/Group 71.png')}}" style="width: 100%" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="light-round" data-animscroll="fade-up" data-animscroll-delay="200"></div>
+</section>
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-3" data-animscroll="fade-up">
+                <h1>Heading 1</h1>
+                <p>
+                    Lorem ipsum eos et accusam et justo duo dolores et ea rebum. Stet
+                    clita kasd gubergren, no sea takimata
+                </p>
+            </div>
+            <div class="col-3" data-animscroll="fade-up" data-animscroll-delay="100">
+                <h1>Heading 2</h1>
+                <ul>
+                    <li><a href="#">Lorem ipsum link</a></li>
+                    <li><a href="#">Lorem ipsum link</a></li>
+                    <li><a href="#">Lorem ipsum link</a></li>
+                    <li><a href="#">Lorem ipsum link</a></li>
+                    <li><a href="#">Lorem ipsum link</a></li>
+                </ul>
+            </div>
+            <div class="col-3" data-animscroll="fade-up" data-animscroll-delay="200">
+                <h1>Heading 2</h1>
+                <ul>
+                    <li><i class="fas fa-map-marker-alt"></i>Lorem ipsum address</li>
+                    <li class="mt-2">
+                        <i class="fas fa-phone-alt"></i>0112 xxxxxxxx
+                    </li>
+                    <li class="mt-2">
+                        <i class="fas fa-envelope"></i>lorem@loremipsum.com
+                    </li>
+                </ul>
+            </div>
+            <div class="col-3" data-animscroll="fade-up" data-animscroll-delay="300">
+                <h1>Follow Us</h1>
+                <br />
+                <div class="row m-0">
+                    <a href="#"> <i class="fab fa-facebook-square icon-lg"></i> </a>
+                    <a href="#"> <i class="fab fa-instagram icon-lg"></i></a>
+                    <a href="#"> <i class="fab fa-linkedin icon-lg"></i></a>
+                    <a href="#"> <i class="fab fa-youtube icon-lg"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Optional JavaScript -->
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<script src="{{url('light_theme/plugin/AnimTrap/js/anim-trap.js')}}"></script>
+<script src="{{url('light_theme/plugin/AnimTrap/js/anim-scroll.js')}}"></script>
+<script src="{{url('light_theme/js/main.js')}}"></script>
+<script>
+    ANIMSCROLL.init({
+        easing: "ease-in-out-sine",
+    });
+</script>
+</body>
+</html>
