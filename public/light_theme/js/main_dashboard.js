c75e63d415548a0c5cc76e04e63190ee47d41394
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-05-01 22:38:40 +0530

    Fonts and Solide Data

---
+++ public/light_theme/js/main_dashboard.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var sc = $(window).scrollTop();
+    if (sc > 90) {
+      $(".navbar-top").addClass("sticky");
+      $(".sticky-logo").removeClass("d-none");
+      $(".default-logo").addClass("d-none");
+    } else {
+      $(".navbar-top").removeClass("sticky");
+      $(".sticky-logo").addClass("d-none");
+      $(".default-logo").removeClass("d-none");
+    }
+  });
+
+  // ---------------------------- Search --------------------
+  var search_status = 0;
+  $("#btn-search").click(function () {
+	if(search_status == 0) {
+		$(".latest-connections").hide();
+		$(".populer-connections").hide();
+		$("#search-result").removeClass("display-none");
+
+		$(".industry").removeClass("display-none");
+		$(".industry-option").removeClass("display-none");
+		$(".industry-search").removeClass("display-none");
+
+		$('#search').removeClass("width-sm");
+		$('#search-filter').removeClass("display-none");
+		$('#btn-search').removeClass("fa-search");
+		$('#search').addClass("width-lg");
+		$('#btn-search').addClass("fa-chevron-right");
+		$('.search-text').addClass("display-none");
+		search_status = 1;
+	}else {
+		$(".latest-connections").show();
+		$(".populer-connections").show();
+		$("#search-result").addClass("display-none");
+
+		$(".industry").addClass("display-none");
+		$(".industry-option").addClass("display-none");
+		$(".industry-search").addClass("display-none");
+
+
+		$('#search').removeClass("width-lg");
+		$('#search-filter').addClass("display-none");
+		$('#btn-search').removeClass("fa-chevron-right");
+		$('#search').addClass("width-sm");
+		$('#btn-search').addClass("fa-search");
+		$('.search-text').removeClass("display-none");
+		search_status = 0;
+	}
+
+  });
+});
