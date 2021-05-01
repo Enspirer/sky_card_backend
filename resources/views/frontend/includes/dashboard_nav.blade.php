517f55cf7cdbdc1d41265ab2c23fbcb582a4a743
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-05-01 22:38:50 +0530

    Dashboard Details

---
+++ resources/views/frontend/includes/dashboard_nav.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar-top sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                <div class="row">
+                    <ul>
+                        <li><a class="navigation-link active" href="{{url('/')}}">Home</a></li>
+                        <li><a class="navigation-link" href="#">Sky Cards App</a></li>
+                        <li><a class="navigation-link" href="#">Sky Club</a></li>
+                        <li><a class="navigation-link" href="#">Sky Daily</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-2 d-flex justify-content-center">
+                <img src="{{url('light_theme/assets/logo compressed.png')}}" class="default-logo header-logo" alt="" />
+                <img src="{{url('light_theme/assets/logo compressed.png')}}" class="sticky-logo d-none" alt="" />
+            </div>
+            <div class="col-5">
+                <div class="row">
+                    <ul>
+                        <li><a class="navigation-link" href="#">Sky Card Holder</a></li>
+                        <li><a class="navigation-link" href="#">Contact</a></li>
+                        <li><a class="navigation-link" href="#">About</a></li>
+                        @auth
+                        <li>
+                            <a class="navigation-link reg" href="{{route('frontend.auth.login')}}">{{auth()->user()->first_name}} {{auth()->user()->last_name}}</a>
+                        </li>
+                        @else
+                            <li>
+                                <a class="navigation-link reg" href="{{route('frontend.auth.login')}}">Register/Log In</a>
+                            </li>
+                            @endauth
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -36,206 +36,15 @@
                             </button>
                         </span>
                 </div>
-                <div class="scrollbar-sidebar">
-                    <div class="app-sidebar__inner">
-                        <ul class="vertical-nav-menu">
-                            <li class="app-sidebar__heading">Dashboards</li>
-                            <li>
-                                <a href="index.html" class="mm-active">
-                                    <i class="metismenu-icon pe-7s-rocket"></i>
-                                    Dashboard Example 1
-                                </a>
-                            </li>
-                            <li class="app-sidebar__heading">UI Components</li>
-                            <li>
-                                <a href="#">
-                                    <i class="metismenu-icon pe-7s-diamond"></i>
-                                    Elements
-                                    <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
-                                </a>
-                                <ul>
-                                    <li>
-                                        <a href="elements-buttons-standard.html">
-                                            <i class="metismenu-icon"></i>
-                                            Buttons
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-dropdowns.html">
-                                            <i class="metismenu-icon">
-                                            </i>Dropdowns
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-icons.html">
-                                            <i class="metismenu-icon">
-                                            </i>Icons
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-badges-labels.html">
-                                            <i class="metismenu-icon">
-                                            </i>Badges
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-cards.html">
-                                            <i class="metismenu-icon">
-                                            </i>Cards
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-list-group.html">
-                                            <i class="metismenu-icon">
-                                            </i>List Groups
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-navigation.html">
-                                            <i class="metismenu-icon">
-                                            </i>Navigation Menus
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-utilities.html">
-                                            <i class="metismenu-icon">
-                                            </i>Utilities
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <i class="metismenu-icon pe-7s-car"></i>
-                                    Components
-                                    <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
-                                </a>
-                                <ul>
-                                    <li>
-                                        <a href="components-tabs.html">
-                                            <i class="metismenu-icon">
-                                            </i>Tabs
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-accordions.html">
-                                            <i class="metismenu-icon">
-                                            </i>Accordions
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-notifications.html">
-                                            <i class="metismenu-icon">
-                                            </i>Notifications
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-modals.html">
-                                            <i class="metismenu-icon">
-                                            </i>Modals
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-progress-bar.html">
-                                            <i class="metismenu-icon">
-                                            </i>Progress Bar
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-tooltips-popovers.html">
-                                            <i class="metismenu-icon">
-                                            </i>Tooltips &amp; Popovers
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-carousel.html">
-                                            <i class="metismenu-icon">
-                                            </i>Carousel
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-calendar.html">
-                                            <i class="metismenu-icon">
-                                            </i>Calendar
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-pagination.html">
-                                            <i class="metismenu-icon">
-                                            </i>Pagination
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-scrollable-elements.html">
-                                            <i class="metismenu-icon">
-                                            </i>Scrollable
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-maps.html">
-                                            <i class="metismenu-icon">
-                                            </i>Maps
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li  >
-                                <a href="tables-regular.html">
-                                    <i class="metismenu-icon pe-7s-display2"></i>
-                                    Tables
-                                </a>
-                            </li>
-                            <li class="app-sidebar__heading">Widgets</li>
-                            <li>
-                                <a href="dashboard-boxes.html">
-                                    <i class="metismenu-icon pe-7s-display2"></i>
-                                    Dashboard Boxes
-                                </a>
-                            </li>
-                            <li class="app-sidebar__heading">Forms</li>
-                            <li>
-                                <a href="forms-controls.html">
-                                    <i class="metismenu-icon pe-7s-mouse">
-                                    </i>Forms Controls
-                                </a>
-                            </li>
-                            <li>
-                                <a href="forms-layouts.html">
-                                    <i class="metismenu-icon pe-7s-eyedropper">
-                                    </i>Forms Layouts
-                                </a>
-                            </li>
-                            <li>
-                                <a href="forms-validation.html">
-                                    <i class="metismenu-icon pe-7s-pendrive">
-                                    </i>Forms Validation
-                                </a>
-                            </li>
-                            <li class="app-sidebar__heading">Charts</li>
-                            <li>
-                                <a href="charts-chartjs.html">
-                                    <i class="metismenu-icon pe-7s-graph2">
-                                    </i>ChartJS
-                                </a>
-                            </li>
-                            <li class="app-sidebar__heading">PRO Version</li>
-                            <li>
-                                <a href="https://dashboardpack.com/theme-details/architectui-dashboard-html-pro/" target="_blank">
-                                    <i class="metismenu-icon pe-7s-graph2">
-                                    </i>
-                                    Upgrade to PRO
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>    <div class="app-main__outer">
+               @include('frontend.includes.sidebar')
+            </div>
+            <div class="app-main__outer">
                 <div class="app-main__inner">
                     <div class="app-page-title">
                         <div class="page-title-wrapper">
                             <div class="page-title-heading">
                                 <div class="page-title-icon">
-                                    <i class="pe-7s-car icon-gradient bg-mean-fruit">
+                                    <i class="fa fa-file">
                                     </i>
                                 </div>
                                 <div>Analytics Dashboard
@@ -277,24 +86,22 @@
                                             <li class="nav-item">
                                                 <a href="javascript:void(0);" class="nav-link">
                                                     <i class="nav-link-icon lnr-picture"></i>
-                                                    <span>
-                                                            Picture
-                                                        </span>
+                                                    <span>Picture</span>
                                                 </a>
                                             </li>
                                             <li class="nav-item">
                                                 <a disabled href="javascript:void(0);" class="nav-link disabled">
                                                     <i class="nav-link-icon lnr-file-empty"></i>
-                                                    <span>
-                                                            File Disabled
-                                                        </span>
+                                                    <span>File Disabled</span>
                                                 </a>
                                             </li>
                                         </ul>
                                     </div>
                                 </div>
-                            </div>    </div>
-                    </div>            <div class="row">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
                         <div class="col-md-6 col-xl-4">
                             <div class="card mb-3 widget-content bg-midnight-bloom">
                                 <div class="widget-content-wrapper text-white">
@@ -379,7 +186,7 @@
                                                             <div class="widget-content p-0">
                                                                 <div class="widget-content-wrapper">
                                                                     <div class="widget-content-left mr-3">
-                                                                        <img width="42" class="rounded-circle" src="assets/images/avatars/9.jpg" alt="">
+                                                                        <img width="42" class="rounded-circle" src="{{url('light_theme/dashboard/assets/images/avatars/9.jpg')}}" alt="">
                                                                     </div>
                                                                     <div class="widget-content-left">
                                                                         <div class="widget-heading">Ella-Rose Henry</div>
@@ -401,7 +208,7 @@
                                                             <div class="widget-content p-0">
                                                                 <div class="widget-content-wrapper">
                                                                     <div class="widget-content-left mr-3">
-                                                                        <img width="42" class="rounded-circle" src="assets/images/avatars/5.jpg" alt="">
+                                                                        <img width="42" class="rounded-circle" src="{{url('light_theme/dashboard/assets/images/avatars/5.jpg')}}" alt="">
                                                                     </div>
                                                                     <div class="widget-content-left">
                                                                         <div class="widget-heading">Ruben Tillman</div>
@@ -423,7 +230,7 @@
                                                             <div class="widget-content p-0">
                                                                 <div class="widget-content-wrapper">
                                                                     <div class="widget-content-left mr-3">
-                                                                        <img width="42" class="rounded-circle" src="assets/images/avatars/4.jpg" alt="">
+                                                                        <img width="42" class="rounded-circle" src="{{url('light_theme/dashboard/assets/images/avatars/4.jpg')}}" alt="">
                                                                     </div>
                                                                     <div class="widget-content-left">
                                                                         <div class="widget-heading">Vinnie Wagstaff</div>
@@ -445,7 +252,7 @@
                                                             <div class="widget-content p-0">
                                                                 <div class="widget-content-wrapper">
                                                                     <div class="widget-content-left mr-3">
-                                                                        <img width="42" class="rounded-circle" src="assets/images/avatars/3.jpg" alt="">
+                                                                        <img width="42" class="rounded-circle" src="{{url('light_theme/dashboard/assets/images/avatars/3.jpg')}}" alt="">
                                                                     </div>
                                                                     <div class="widget-content-left">
                                                                         <div class="widget-heading">Ella-Rose Henry</div>
@@ -467,7 +274,7 @@
                                                             <div class="widget-content p-0">
                                                                 <div class="widget-content-wrapper">
                                                                     <div class="widget-content-left mr-3">
-                                                                        <img width="42" class="rounded-circle" src="assets/images/avatars/2.jpg" alt="">
+                                                                        <img width="42" class="rounded-circle" src="{{url('light_theme/dashboard/assets/images/avatars/2.jpg')}}" alt="">
                                                                     </div>
                                                                     <div class="widget-content-left">
                                                                         <div class="widget-heading">Ruben Tillman</div>
