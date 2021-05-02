f77f7c6854cffdd38737474bd6a51dc855b22420
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-05-02 16:05:26 +0530

    Company Card Item

---
+++ resources/views/frontend/user/companies/sections/company_card_item_draft.blade.php
@@ -0,0 +1,36 @@
+<div class="col-md-4">
+    <div class="text-center card-box">
+        <div class="member-card pt-2 pb-2">
+            <div class="thumb-lg member-thumb mx-auto">
+                <img src="{{url('img/frontend/box2.jpg')}}" class="rounded-circle img-thumbnail" style="object-fit: contain;height: 80px;width: 80px;" alt="profile-image"></div>
+            <div class="">
+                <h4>{{$pb_company_title}}</h4>
+                <p class="text-muted">{{$pb_company_category}} <span>| </span><span><a href="#" class="text-pink">{{$pb_company_website}}</a></span></p>
+            </div>
+            <button type="button" class="btn btn-primary mt-3 btn-rounded waves-effect w-md waves-light">View Company</button>
+            <button type="button" class="btn btn-primary mt-3 btn-rounded waves-effect w-md waves-light">Dashboard</button>
+            <div class="mt-4">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="mt-3">
+                            <h4>23</h4>
+                            <p class="mb-0 text-muted">Employees</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="mt-3">
+                            <h4>6952</h4>
+                            <p class="mb-0 text-muted">Following</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="mt-3">
+                            <h4>1125</h4>
+                            <p class="mb-0 text-muted">Reach Count</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
