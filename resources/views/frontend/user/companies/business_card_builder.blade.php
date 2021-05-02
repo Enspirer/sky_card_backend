6c81a0b39d344d78137352c5cb21cc3da492b8d5
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-05-02 19:00:50 +0530

    Business card Builder

--- resources/views/frontend/user/companies/business_card_builder.blade.php
+++ resources/views/frontend/user/companies/business_card_builder.blade.php
@@ -2,23 +2,22 @@
 
 @section('title', app_name() . ' | ' . __('navs.frontend.dashboard') )
 
-
+<link href="{{url('light_theme/step_p/style.css')}}" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 
 @section('content')
     <div class="app-main__outer">
         <div class="app-main__inner">
             @include('frontend.user.dashboard_components.title_bar',[
-            'title_icon' => 'fa fa-building',
-            'title_description' => 'You can create your own company page with SkyCards',
-            'title_name'=>'Create Company']
+            'title_icon' => 'fa fa-id-card',
+            'title_description' => 'You can create your own company business card with SkyCards',
+            'title_name'=>'Create Business Card']
             )
-
-            <div class="row">
-
-            </div>
-
+            @include('frontend.user.companies.sections.card_builder_steps_section',['step' => 1])
         </div>
     </div>
+
+    <script src="{{url('light_theme/step_p/script.js')}}"></script>
 
 
 @endsection
