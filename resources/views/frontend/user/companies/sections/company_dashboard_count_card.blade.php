d96f7dd36b7d364b97773b4dce4aa090782b070e
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-05-02 17:57:50 +0530

    Company Dashboard Count cards

---
+++ resources/views/frontend/user/companies/sections/company_dashboard_count_card.blade.php
@@ -0,0 +1,41 @@
+<div class="no-gutters row">
+    <div class="col-md-4">
+        <div class="widget-content">
+            <div class="widget-content-wrapper">
+                <div class="widget-content-right ml-0 mr-3">
+                    <div class="widget-numbers text-success">00</div>
+                </div>
+                <div class="widget-content-left">
+                    <div class="widget-heading">Views</div>
+                    <div class="widget-subheading">Last year expenses</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="widget-content">
+            <div class="widget-content-wrapper">
+                <div class="widget-content-right ml-0 mr-3">
+                    <div class="widget-numbers text-warning">00</div>
+                </div>
+                <div class="widget-content-left">
+                    <div class="widget-heading">Action Calls</div>
+                    <div class="widget-subheading">Total</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="widget-content">
+            <div class="widget-content-wrapper">
+                <div class="widget-content-right ml-0 mr-3">
+                    <div class="widget-numbers text-danger">00</div>
+                </div>
+                <div class="widget-content-left">
+                    <div class="widget-heading">Published Posts</div>
+                    <div class="widget-subheading">Last Week</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
