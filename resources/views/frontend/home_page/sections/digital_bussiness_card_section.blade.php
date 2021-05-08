d694917bdafa30ee78ea90c8e1c219acc6347ab4
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-05-09 01:15:30 +0530

    Digital Bussines card

--- resources/views/frontend/home_page/sections/digital_bussiness_card_section.blade.php
+++ resources/views/frontend/home_page/sections/digital_bussiness_card_section.blade.php
@@ -3,11 +3,11 @@
         <div class="row">
 
             <div class="col-6">
-                <img data-animscroll="fade-left" src="assets/Group 330.png" alt="" class="animscroll-init animscroll-animate">
+                <img data-animscroll="fade-left" src="{{url('light_theme/assets/Group 330.png')}}" alt="" class="animscroll-init animscroll-animate">
             </div>
 
             <div class="col-6">
-                <img src="assets/Group 545.svg" width="50%" style="width: 60%;" class="mt-5" alt="">
+                <img src="{{url('light_theme/assets/Group 545.svg')}}" width="50%" style="width: 60%;" class="mt-5" alt="">
 
                 <p data-animscroll="fade-up" data-animscroll-delay="200" class="animscroll-init animscroll-animate">
                     Select from our card designs or create your <br>
