7d17e30b4cff37b7989985d033ddb3c150febd30
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-05-08 12:48:03 +0530

    Text Blades

--- resources/views/frontend/user/portfolio/index.blade.php
+++ resources/views/frontend/user/portfolio/index.blade.php
@@ -26,7 +26,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a role="tab" class="nav-link show" id="tab-2" data-toggle="tab" href="#tab-content-2" aria-selected="false">
+                            <a role="tab" class="nav-link show" id="tab-2" data-toggle="tab" href="#tab-content-3" aria-selected="false">
                                 <span>Website Links</span>
                             </a>
                         </li>
@@ -37,6 +37,23 @@
                                 <div class="col-md-3">
                                     <div style="padding: 20px;border-style: dashed;border-color: grey;text-align: center;padding-top: 120px;padding-bottom: 110px;" data-toggle="modal" data-target="#photo_upload">Add Photos</div>
                                 </div>
+                                @foreach($items as $image_item)
+                                    @if($image_item->content_type == 'Images')
+                                        <div class="col-md-4">
+                                            <div class="card" style="">
+                                                <div class="card-image">
+                                                    <div style="background-image: url('{{$image_item->photo_link}}');height: 250px;background-size: contain;background-repeat: no-repeat;background-position: center;border-style: groove;"></div>
+                                                </div>
+
+                                                <div class="card-body">
+                                                    <span class="card-title">{{$image_item->caption}}</span>
+                                                </div><!-- card content -->
+                                            </div>
+                                        </div>
+                                    @else
+
+                                    @endif
+                                @endforeach
                             </div>
                         </div>
                         <div class="tab-pane tabs-animation fade" id="tab-content-2" role="tabpanel">
@@ -52,12 +69,10 @@
                                                     <div class="embed-responsive embed-responsive-16by9">
                                                         <iframe width="560" height="315" src="https://www.youtube.com/embed/{{$video_items->youtube_link}}" frameborder="0" allowfullscreen></iframe>
                                                     </div>
-
-                                                </div><!-- card image -->
+                                                </div>
 
                                                 <div class="card-body">
                                                     <span class="card-title">{{$video_items->caption}}</span>
-
                                                 </div><!-- card content -->
                                             </div>
                                         </div>
@@ -70,6 +85,20 @@
 
                             </div>
                         </div>
+
+                        <div class="tab-pane tabs-animation fade" id="tab-content-3" role="tabpanel">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div style="padding: 20px;border-style: dashed;border-color: grey;text-align: center;padding-top: 120px;padding-bottom: 110px;" data-toggle="modal" data-target="#website_links_dialog">Add Website</div>
+                                </div>
+                                @foreach($items as $website_link)
+                                    @if($website_link->content_type == 'Website Links')
+
+                                    @endif
+                                @endforeach
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
@@ -80,6 +109,7 @@
     @push('dialog-push')
         @include('frontend.user.portfolio.dialogs.video_upload_dialog')
         @include('frontend.user.portfolio.dialogs.photo_upload_dialog')
+        @include('frontend.user.portfolio.dialogs.website_links_dialog')
 
     @endpush
 @endsection
